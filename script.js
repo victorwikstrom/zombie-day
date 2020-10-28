b173eb6843e8211e95ff44ea5304043cc49b4737
victorwikstrom <victor.wikstrom@icloud.com>
2020-10-28 17:45:36 +0100

    Tried to create a button to loop through infoList array, but got stuck...

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const playerNameElement = document.getElementById('player-name')
 const roomImageElement = document.getElementById('room-image')
 const roomInfoElement = document.getElementById('room-info')
-const buttonElement = document.getElementById('option-buttons') // NOTE! Grid element, not button
+const navGrid = document.getElementById('nav-grid') 
 
 
 /* ------------------------------- FUNCTIONS -------------------------------- */
@@ -13,65 +13,85 @@ const buttonElement = document.getElementById('option-buttons') // NOTE! Grid el
  * Initates the game, resets the state of the player and loads the first room 
  */
  function initGame() {
-  // setPlayer()
   loadRoom(1)
  }
 
- function setPlayer() {
-    const playerName = prompt("Please type your name")
-    playerNameElement.innerText = playerName
+function loadRoom(roomId) {
+
+  let i = 0
+
+  const currentRoom = roomList.find(currentRoom => currentRoom.id === roomId)
+  const roomInfo = currentRoom.infoList[1].text
+  roomInfoElement.innerText = roomInfo
+
+  const nextButton = document.createElement('button')
+  nextButton.innerText = '>'
+  nextButton.classList.add('button')
+  roomInfoElement.appendChild(nextButton)
+
+  nextButton.addEventListener('click', () => updateIndex() )
+    
   }
 
- /**
- * Loads a new room based on roomList index
- * @param {*} roomIndex
- */
-function loadRoom(roomIndex) {
-
-  // Finds the current room in the room list and loads room info into the html element
-  const roomInfo = roomList.find(roomInfo => roomInfo.id === roomIndex)
-  roomInfoElement.innerText = roomInfo.info
-
-  // Removes all buttons
-  while (buttonElement.firstChild) {
-    buttonElement.removeChild(buttonElement.firstChild)
-  }
-
-  // Loop through the optionList-array and adds the all found items to the button-grid
-  for (let option of roomInfo.optionsList) {
-    if (showOption(option)) {
-      const button = document.createElement('button') // Create the new button element
-      button.innerText = option.text // Set the text from the optionList-array  
-      button.classList.add('button') // Set css styling to the button
-      button.addEventListener('click', () => selectOption(option)) // Set click-event to option 
-      buttonElement.appendChild(button) // Add the button to the DOM
-    }
-  }
+function updateIndex() {
+  console.log('test')
 }
 
-function showOption(option) {
-return true
+
+while (navGrid.firstChild) {
+  navGrid.removeChild(navGrid.firstChild)
+}
+while (roomInfoElement.firstChild) {
+  roomInfoElement.removeChild(roomInfoElement.firstChild)
 }
 
-function selectOption(option) {
-  const nextRoomId = option.nextRoom 
-  loadRoom(nextRoomId) // Load new room info as set in the optionsList array
-}
+
+
 
 /* -------------------------------- GAME ENVIRONMENT -------------------------------- */
 
 const roomList = [
   {
     id: 1,
-    info: 'This is the first room',
-    optionsList: [
-      { 
-        text: 'Go to room 2',
-        nextRoom: 2
+    infoList: [
+      {
+        text: 'This is the first info object in room 1',
+        textId: 1,
+        nextTextId: 2,
+        optionList: [
+          { text: 'Go to room 2',
+            nextRoom: 2
+          },
+          { text: 'Go to room 3',
+            nextRoom: 3
+          }
+        ]
       },
-      { 
-        text: 'Go to room 3',
-        nextRoom: 3
+      {
+        text: 'This is the second info object in room 1',
+        textId: 2,
+        nextTextId: 3,
+        optionList: [
+          { text: 'Go to room 2',
+            nextRoom: 2
+          },
+          { text: 'Go to room 3',
+            nextRoom: 3
+          }
+        ]
+      },
+      {
+        text: 'This is the third info object in room 1',
+        textId: 3,
+        nextTextId: 3,
+        optionList: [
+          { text: 'Go to room 2',
+            nextRoom: 2
+          },
+          { text: 'Go to room 3',
+            nextRoom: 3
+          }
+        ]
       }
     ]
   },
