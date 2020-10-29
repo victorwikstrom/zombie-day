c12130f7cb26a3bb05048c5c7e83eb95e0203778
victorwikstrom <victor.wikstrom@icloud.com>
2020-10-29 11:57:27 +0100

    Got he button to show next info of one room to function correctly

--- script.js
+++ script.js
@@ -9,41 +9,46 @@ const navGrid = document.getElementById('nav-grid')
 
 /* ------------------------------- FUNCTIONS -------------------------------- */
 
-/**
- * Initates the game, resets the state of the player and loads the first room 
- */
+/** Initates the game loads the room number one. */
  function initGame() {
   loadRoom(1)
  }
 
+ /** Loads the room the user is currently in and display the first info string. */
 function loadRoom(roomId) {
-
-  let i = 0
-
   const currentRoom = roomList.find(currentRoom => currentRoom.id === roomId)
-  const roomInfo = currentRoom.infoList[1].text
+  removeElements()
+  loadRoomInfo(currentRoom, 0) 
+}
+
+/** Removes all elements from navGridElement and roomInfoElement. */
+function removeElements() {
+  while (navGrid.firstChild) {
+    navGrid.removeChild(navGrid.firstChild)
+  }
+  while (roomInfoElement.firstChild) {
+    roomInfoElement.removeChild(roomInfoElement.firstChild)
+  }
+}
+
+/** Loads the info of the current room based on the index on the infoList array. */
+function loadRoomInfo(currentRoom, infoIndex) {
+  const roomInfo = currentRoom.infoList[infoIndex].text
   roomInfoElement.innerText = roomInfo
 
   const nextButton = document.createElement('button')
-  nextButton.innerText = '>'
+  nextButton.innerText = '>>'
   nextButton.classList.add('button')
   roomInfoElement.appendChild(nextButton)
 
-  nextButton.addEventListener('click', () => updateIndex() )
-    
-  }
-
-function updateIndex() {
-  console.log('test')
+  nextButton.addEventListener('click', () => showNextInfo(currentRoom, infoIndex))
 }
 
+function showNextInfo(currentRoom, infoIndex) {
+  let nextIndex = infoIndex + 1
+  loadRoomInfo(currentRoom, nextIndex)
+}
 
-while (navGrid.firstChild) {
-  navGrid.removeChild(navGrid.firstChild)
-}
-while (roomInfoElement.firstChild) {
-  roomInfoElement.removeChild(roomInfoElement.firstChild)
-}
 
 
 
@@ -55,40 +60,20 @@ const roomList = [
     id: 1,
     infoList: [
       {
-        text: 'This is the first info object in room 1',
-        textId: 1,
-        nextTextId: 2,
-        optionList: [
-          { text: 'Go to room 2',
-            nextRoom: 2
-          },
-          { text: 'Go to room 3',
-            nextRoom: 3
-          }
-        ]
+        text: 'This is the first info object in room 1'
       },
       {
-        text: 'This is the second info object in room 1',
-        textId: 2,
-        nextTextId: 3,
-        optionList: [
-          { text: 'Go to room 2',
-            nextRoom: 2
-          },
-          { text: 'Go to room 3',
-            nextRoom: 3
-          }
-        ]
+        text: 'This is the second info object in room 1'
       },
       {
         text: 'This is the third info object in room 1',
-        textId: 3,
-        nextTextId: 3,
         optionList: [
-          { text: 'Go to room 2',
-            nextRoom: 2
+          {
+            text: 'Go back to room 1',
+            nextRoom: 1
           },
-          { text: 'Go to room 3',
+          {
+            text: 'Go to room 3',
             nextRoom: 3
           }
         ]
