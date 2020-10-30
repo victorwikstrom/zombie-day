69ec8d4c122e3214e11489bc84a647cdf9b03953
victorwikstrom <victor.wikstrom@icloud.com>
2020-10-30 16:50:27 +0100

    Added game story, input field for name and final styling

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 /* ------------------------------- HTML Elements -------------------------------- */
 
 const playerNameElement = document.getElementById('player-name')
+const headerElement = document.getElementById('game-header')
 const roomImageElement = document.getElementById('room-image')
 const roomInfoElement = document.getElementById('room-info')
 const nextElement = document.getElementById('next-container')
 const navGrid = document.getElementById('nav-grid') 
 const roomNameElement = document.getElementById('room-name')
+const nameInput = document.getElementById('name-input')
 
 // Add css styling to some text elements 
 roomInfoElement.classList.add('info-text')
@@ -15,21 +17,39 @@ roomNameElement.classList.add('info-text', 'yellow-text')
 
 /* ------------------------------- FUNCTIONS ----------------------------------- */
 
-/** Initates the game loads the room number one. */
+/** Initates the game, sets player name by prompt, and creates button to load the first room. */
  function startGame() {
-  loadRoom('bedroom')
+   const button = document.createElement('button')
+   button.innerText = 'Start game'
+   button.classList.add('button')
+   roomInfoElement.appendChild(button)
+   button.addEventListener('click', () => loadRoom('bedroom'))
  }
 
- /** Loads the room the user is currently in and display the first info string. */
-function loadRoom(roomId) {
-  const currentRoom = roomList.find(currentRoom => currentRoom.roomId === roomId)
-  roomNameElement.innerText = currentRoom.roomName
+ /**
+  * Gets the name of the player from the input field.
+  * @returns {string} Name of the player as a string
+  */
+ function getPlayerName() {
+   return nameInput.value
+ }
 
+  
+
+ /**
+  * Loads the room the user is currently in and display the first info string. 
+  * @param {string} roomId The string id of the room to load
+  */
+function loadRoom(roomId) {
+  const gameHeader = getPlayerName()
+  headerElement.innerText = gameHeader + "'s Zombie Day"
+  const currentRoom = roomList.find(currentRoom => currentRoom.roomId === roomId)
   removeElements()
+  roomNameElement.innerText = currentRoom.roomName
   loadRoomInfo(currentRoom, 0) 
 }
 
-/** Removes all elements from navGridElement and roomInfoElement. */
+/** Clears the GUI from all elements */
 function removeElements() {
   while (navGrid.firstChild || roomInfoElement.firstChild) {
     navGrid.innerHTML = ''
@@ -39,8 +59,8 @@ function removeElements() {
 
 /**
  * Loads the info of the current room based on the index on the infoList array
- * @param {Object} currentRoom 
- * @param {Number} infoIndex 
+ * @param {Object} currentRoom Room object where user currently is
+ * @param {Number} infoIndex Index of the info text in infoList array
  */
 function loadRoomInfo(currentRoom, infoIndex) {
     const roomInfo = currentRoom.infoList[infoIndex].text
@@ -50,7 +70,7 @@ function loadRoomInfo(currentRoom, infoIndex) {
 
 /**
  * Creates the button for displaying the next info text in the infoList array
- * @param {Object} currentRoom 
+ * @param {Object} currentRoom Room object where user currently is
  * @param {Number} infoIndex 
  */
 function createNextButton(currentRoom, infoIndex) {
@@ -62,19 +82,17 @@ function createNextButton(currentRoom, infoIndex) {
 }
 
 /**
- * Shows the next info text of a room. If the info text is the last of the room, option buttons will load. 
- * @currentRoom {Object} *Represent the users current room 
- * @param {Number} infoIndex 
+ * Loads the next info object in the infoList array. If the index of the array object is less than the length of the array, the option buttons will load. 
+ * @param {Object} currentRoom Room object where user currently is
+ * @param {Number} infoIndex Index of the info text in infoList array
  */
 function showNextInfo(currentRoom, infoIndex) {
   if (infoIndex < currentRoom.infoList.length - 1) {
     infoIndex += 1
     loadRoomInfo(currentRoom, infoIndex)
-    console.log(infoIndex)
-    console.log(currentRoom.infoList.length - 1)
   }
   else {
-    roomInfoElement.innerText = 'What should you do next?'
+    roomInfoElement.innerText = 'What do you want to do next?'
     loadOptionButtons(currentRoom, infoIndex)
   }
 }
@@ -92,7 +110,18 @@ function loadOptionButtons(currentRoom, infoIndex) {
     optionButton.innerText = option.text
     optionButton.classList.add('button')
     navGrid.appendChild(optionButton)
-    optionButton.addEventListener('click', () => loadRoom(nextRoom))
+    optionButton.addEventListener('click', () => selectOption(nextRoom))
+  }
+}
+
+function selectOption(nextRoom) {
+  if (nextRoom === 'exit') {
+    removeElements()
+    roomNameElement.innerText = 'Thank you for playing Zombie Day'
+    roomInfoElement.innerText = 'You may now close the browser window.'
+  }
+  else {
+    loadRoom(nextRoom)
   }
 }
 
@@ -100,6 +129,7 @@ function loadOptionButtons(currentRoom, infoIndex) {
 
 /* -------------------------------- GAME ENVIRONMENT -------------------------------- */
 
+/** List of all room objects. Highest in array hierarchy */
 const roomList = [
   {
     roomId: 'bedroom',
@@ -109,17 +139,17 @@ const roomList = [
         text: 'You wake up in your bedroom. The birds are chirping and the sun is already shining. What a lovely day! You reach for your phone next to your bed and check the notifications. The first thing you see  give you chills all over your body. You had completely forgot...'
       },
       {
-        text: 'ITS FREAKING ZOMBIE DAY!!! The terrifying but also low-key exciting day of the year where everyone, except for yourself has turned into zombies for one day. You had completely forgot about this... and havent had any time to prepare or anything... '
+        text: '... ITS FREAKING ZOMBIE DAY!!! The terrifying but also low-key exciting day of the year where everyone, except for yourself has turned into zombies for one day. You had completely forgot about this... and havent had any time to prepare or anything... '
       },
       {
-        text: 'You listen carefully to your surroundings... Except for the happy birds outside, you can now hear your zombie room mate is lurking in the living room outside. What should you do next?',
+        text: '... You listen carefully to your surroundings. Except for the happy birds outside, you can now hear your zombie room mate is lurking in the living room outside. Oh my, imagine having to kill your roomie...',
         optionList: [
           {
             text: 'Walk out to the living room',
             nextRoom: 'living-room'
           },
           {
-            text: 'Sneak out the bedroom window (you dont want to risk having to kill your room mate, as he is (normally) a pretty nice guy)',
+            text: 'Sneak out the bedroom window',
             nextRoom: 'garden'
           }
         ]
@@ -131,18 +161,43 @@ const roomList = [
     roomName: 'The Living Room',
     infoList: [
       {
-        text: 'You open the door to the living room and can see your room mate.'
+        text: 'You open the door to the living room and can see your room mate...'
       },
       {
-        text: 'He starts walking rapidly towards you. In panic you walk backwards look around you for tools and espace routes. What should yo do next?',
+        text: '.. He starts walking rapidly towards you. In panic you walk backwards look around for tools or ways to escape...',
         optionList: [
           {
-            text: 'Try to run past him without getting caught',
-            nextRoom: 3
+            text: 'Try to rush by your room mate out into the garden',
+            nextRoom: 'garden'
           },
           {
-            text: 'Grab hold of the tv remote next to you and try to fight back your room mate',
-            nextRoom: 3
+            text: 'Grab the something close to you and try to fight back your room mate',
+            nextRoom: 'fight-roomie'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'fight-roomie',
+    roomName: 'The Living Room',
+    infoList: [
+      {
+        text: 'You grab the TV remote thats next to you while your roomie is rushing towards you...'
+      },
+      {
+        text: '.. But wait, how could anyone... ANYONE... slay a zombie with a TV-remote?!? Stupid descision...'
+      },
+      {
+        text: 'While you look stupid with your zombie slaying device, your roomie attacks you with full force. Everything turns black and you get eaten. Game over son...',
+        optionList: [
+          {
+            text: 'Restart game',
+            nextRoom: 'bedroom'
+          },
+          {
+            text: 'Exit game',
+            nextRoom: 'exit'
           }
         ]
       }
@@ -153,36 +208,284 @@ const roomList = [
     roomName: 'The Garden',
     infoList: [
       {
-        text: 'Room three'
+        text: 'You escaped and are now in the garden outside of your house. You scan your surroundings for any other zombies...'
       },
       {
-        text: 'ITS FREAKING ZOMBIE DAY!!! The terrifying but also slightly exciting day of the year where everyone, except for yourself has turned into zombies for one day. You had completely forgot about this... and havent had any time to prepare or anything... '
-      },
-      {
-        text: 'You listen carefully to your surroundings... Except for the happy birds outside, you can now hear your zombie room mate is lurking in the living room outside.',
+        text: '... The coast seems clear and you walk slowly out into the streets. A few blocks up the street you know there is a hardware store where you might be able to get some survival supplies. Also, a few blocks down the other way there is a park where you might find a good place to hide.',
         optionList: [
           {
-            text: 'Walk out to the living room',
-            nextRoom: 2
+            text: 'Try to get to the hardware shop to get supplies',
+            nextRoom: 'outside-store'
           },
           {
-            text: 'Sneak out the bedroom window. You dont want to risk having to kill your room mate, as he is (normally) a pretty nice guy',
-            nextRoom: 3
+            text: 'Try to get to the park and find a place to hide',
+            nextRoom: 'park'
           }
         ]
       }
     ]
   },
   {
-    roomId: 'death',
-    roomName: '',
+    roomId: 'outside-store',
+    roomName: 'Outside of the Hardware Store',
     infoList: [
       {
-        text: 'You are dead. Eaten alive. Fuck zombie day...',
+        text: 'You found your way to the hardware store without getting eaten and are now standing outside its windows. '
+      },
+      {
+        text: 'You try to take a look inside the store from the window, but its too dark. If there are already zombies in there, it will be very dangerous to go inside...'
+      },
+      {
+        text: '... but also, without tools you are easy breakfast for the zombies.',
+        optionList: [
+          {
+            text: 'Risk it, and walk into the store via the front door',
+            nextRoom: 'inside-store'
+          },
+          {
+            text: 'Skip the store and run back to the park',
+            nextRoom: 'park'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'inside-store',
+    roomName: 'Inside of the Hardware Store',
+    infoList: [
+      {
+        text: 'You carefully open the door to the store. Eveything seems quiet in there and you walk in.'
+      },
+      {
+        text: 'You close the door behind you even more carefully and try to adjust your eyes to the darkness.'
+      },
+      {
+        text: 'All of the sudden... you hear a terrible gurgling sound behind you and see a massive zombie man walking towards you. F*ck, its zombie Mike, the store owner...'
+      },
+      {
+        text: 'You would stand a chance at fighting him, so you quickly run the other way with Mike just behind you. On the other side of the store there are some shelves with different gardening tools.'
+      },
+      {
+        text: 'You run to the shelve and look for something to defend yourself with',
+        optionList: [
+          {
+            text: 'Pick up a sledge hammer',
+            nextRoom: 'hammer'
+          },
+          {
+            text: 'Pick up a chainsaw',
+            nextRoom: 'chainsaw'
+          },
+          {
+            text: 'Pick up a digging spade',
+            nextRoom: 'spade'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'hammer',
+    roomName: 'Inside the Hardware Store',
+    infoList: [
+      {
+        text: 'You pick up the sledge hammer. Its heavy but yo try to take a big swing towards Zombie Mikes head...'
+      },
+      {
+        text: '... but the hammer misses his head by just a few centimeters and just push Zombie Mike back. He looks at you with his murky eyes and launch himself towards you...',
+      },
+      {
+        text: '... Mike grabs your head and everything turns black. Game over son...',
         optionList: [
           {
             text: 'Restart game',
-            nextRoom: 1
+            nextRoom: 'bedroom'
+          },
+          {
+            text: 'Exit game',
+            nextRoom: 'exit'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'chainsaw',
+    roomName: 'Inside the Hardware Store',
+    infoList: [
+      {
+        text: 'You pick up the chainsaw and look down on it. Damn it, you just realized you have no idea how these work...'
+      },
+      {
+        text: '... while you try to figure out a way to start it Zombie Mike has already reached you...',
+      },
+      {
+        text: '... Mike grabs your head and everything turns black. Game over son...',
+        optionList: [
+          {
+            text: 'Restart game',
+            nextRoom: 'bedroom'
+          },
+          {
+            text: 'Exit game',
+            nextRoom: 'exit'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'spade',
+    roomName: 'Inside the Hardware Store',
+    infoList: [
+      {
+        text: 'You pick up the spade and look at Mike, walking towards you...'
+      },
+      {
+        text: '... and when he is only a few feet away you throw the spade towards Mikes head...',
+      },
+      {
+        text: '... BULLSEYE BICH! The spade perfectly pierce Mikes head right in between the eyes and he slowly fall down on the ground just in front of you. God damn it, sorry Mike. Such a nice guy, normally...',
+      }, 
+      {
+        text: 'You walk out the door of the Hardware Shop again, emotionally disrupted by the killing of Mike and start walking.',
+        optionList: [
+          {
+            text: 'Walk towards the park to try and find a place to hide',
+            nextRoom: 'park'
+          },
+          {
+            text: 'Walk the other way, towards the city',
+            nextRoom: 'city'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'city',
+    roomName: 'The City',
+    infoList: [
+      {
+        text: 'After about ten minutes of walking quietly towards the city, you start to hear something...'
+      },
+      {
+        text: '... Out of the bushes next to you four zombie children starts rushing towards you. F*CK! You scream and starts running in the opposite direction...',
+      },
+      {
+        text: '... but its too late. One of the zombie children has leeched onto your leg and bites...',
+      }, 
+      {
+        text: '... F*CK!!! The pain... its too much. You cant run any longer and fall down while other zombie children jumps onto you.',
+      },
+      {
+        text: 'Eveything turns black. Game over son...',
+        optionList: [
+          {
+            text: 'Restart game',
+            nextRoom: 'bedroom'
+          },
+          {
+            text: 'Exit game',
+            nextRoom: 'exit'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'park',
+    roomName: 'The Park',
+    infoList: [
+      {
+        text: 'You reach the park and can see some zombies walking in the distance. Luckily the park is quite big, so its easy to look out for approachers...'
+      },
+      {
+        text: '... Anyways, you pick up a thick wooden branch to have somehting to defend yourself with, just in case...',
+      },
+      {
+        text: '... Suddenly, two zombies walk out from the public toilett a few meters next to you. You look around and see a gorve of trees about 50 meters from you',
+        optionList: [
+          {
+            text: 'Fight back the zombies with your wooden stick',
+            nextRoom: 'park-fight'
+          },
+          {
+            text: 'Run towards the trees',
+            nextRoom: 'tree-grove'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'tree-grove',
+    roomName: 'The Park',
+    infoList: [
+      {
+        text: 'You made it to the grove of trees, but the zombies are not far behind...'
+      },
+      {
+        text: '... You run a little further into the grove and try to hide...',
+      },
+      {
+        text: '... but the zombies are getting too close...',
+        optionList: [
+          {
+            text: 'Climb up a tree',
+            nextRoom: 'new-day'
+          },
+          {
+            text: 'Fight the zombies with your branch',
+            nextRoom: 'park-fight'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'park-fight',
+    roomName: 'The Park',
+    infoList: [
+      {
+        text: 'You run towards the zombies with the branch in your hands, ready to deliver the first strike...'
+      },
+      {
+        text: '... You hit the first zombie in the head with full force...',
+      },
+      {
+        text: '... Perfect strike in the zombies head. But the branch has split in two pieces and the zombie just look at you as if it was surprised. The branch was rotten. God damn it...'
+      },
+      {
+        text: '... You try to fight back with the small piece of branch thats left, but theres no point. The zombies are to strong. You fall backwards and everything turns black. Game over son...',
+        optionList: [
+          {
+            text: 'Restart game',
+            nextRoom: 'bedroom'
+          },
+          {
+            text: 'Exit game',
+            nextRoom: 'exit'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    roomId: 'new-day',
+    roomName: 'YOU SURVIVED!',
+    infoList: [
+      {
+        text: 'Huge congrats, you made it up the tree and stayed there for the rest of the day. Turns out zombies cant climb trees and you have officially survived Zombie Day. See you next year!',
+        optionList: [
+          {
+            text: 'Restart game',
+            nextRoom: 'bedroom'
+          },
+          {
+            text: 'Exit',
+            nextRoom: 'exit'
           }
         ]
       }
